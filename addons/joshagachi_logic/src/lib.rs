168502eefc1ea7e920045ad6c4e006d507482a03
LegoGuy32109 <joshevanhale@gmail.com>
2025-03-26 20:09:03 -0400

    update to 4.4.1

--- addons/joshagachi_logic/src/lib.rs
+++ addons/joshagachi_logic/src/lib.rs
@@ -4,3 +4,35 @@ struct MyExtension;
 
 #[gdextension]
 unsafe impl ExtensionLibrary for MyExtension {}
+
+#[derive(GodotClass)]
+#[class(base=Node2D)]
+struct Monster {
+    name: String,
+    hitpoints: i32,
+
+    base: Base<Node2D>,
+}
+
+#[godot_api]
+impl INode2D for Monster {
+    fn init(base: Base<Node2D>) -> Self {
+        Self {
+            name: "< Mystery Monster >".into(),
+            hitpoints: 100,
+            base,
+        }
+    }
+
+    fn to_string(&self) -> GString {
+        let Self {
+            name, hitpoints, ..
+        } = &self;
+        godot_print!("{hitpoints}");
+        format!("Monster(name={name}, hp={hitpoints})").into()
+    }
+
+    // fn on_notification(&mut self, what: CanvasItemNotification) {
+    //     godot_print!("Monster {:?}", what)
+    // }
+}
