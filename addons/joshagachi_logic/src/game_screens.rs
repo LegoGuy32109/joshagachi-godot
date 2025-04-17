a4904c90f7ee26d335f24b41fe24e4bd8c8968a9
LegoGuy32109 <joshevanhale@gmail.com>
2025-04-16 22:13:58 -0400

    Snap to center if screen changed during transition, cleaned up find_node func, transitions start from control nodes set by anchors

--- addons/joshagachi_logic/src/game_screens.rs
+++ addons/joshagachi_logic/src/game_screens.rs
@@ -1,7 +1,6 @@
-use crate::PackedScene;
 use godot::classes::tween::{EaseType, TransitionType};
 use godot::classes::{
-    BaseButton, ColorRect, Control, DisplayServer, IControl, ProjectSettings, Tween,
+    BaseButton, ColorRect, Control, DisplayServer, IControl, Panel, ProjectSettings, Tween,
 };
 use godot::obj::WithUserSignals;
 use godot::prelude::*;
@@ -43,13 +42,9 @@ impl IControl for GameScreens {
         godot_print!("Actual Resolution: {}", self.screen_dimensions);
 
         // Find nodes in scene tree
-        let mut landing_screen = game_screens
-            .find_node_on::<Control>("%landing_screen")
-            .expect("issue finding %landing_screen");
+        let mut landing_screen = game_screens.find_node::<Control>("%landing_screen");
 
-        let mut start_game_button = landing_screen
-            .find_node_on::<BaseButton>("%start_game_button")
-            .expect("issue finding %start_game_button");
+        let mut start_game_button = landing_screen.find_node::<BaseButton>("%start_game_button");
 
         // Attach signals
         start_game_button
@@ -61,13 +56,8 @@ impl IControl for GameScreens {
             .connect_self(Self::_on_change_scenes);
 
         // Title Animation
-        landing_screen.set(
-            "position",
-            &(self.viewport_dimensions * Vector2::new(0.5, 0.5)).to_variant(),
-        );
         landing_screen.set("scale", &Vector2::ZERO.to_variant());
-        let mut start_anim_tween =
-            create_tween(game_screens, TransitionType::BOUNCE, EaseType::OUT);
+        let mut start_anim_tween = self.create_tween(TransitionType::BOUNCE, EaseType::OUT);
         start_anim_tween.tween_property(&landing_screen, "scale", &Vector2::ONE.to_variant(), 2.);
     }
 }
@@ -91,16 +81,19 @@ impl GameScreens {
             viewport_dimensions,
             ..
         } = *self;
+        let mut godot_ref = self.to_gd();
         // in seconds
         let duration = 1.0;
 
         // positions stored as variant references for tween_property arguments
-        let new_focus_node_start_position =
-            &(viewport_dimensions * Vector2::new(1.6, 0.5)).to_variant();
-        let screen_center_focus_position =
-            &(viewport_dimensions * Vector2::new(0.5, 0.5)).to_variant();
-        let current_focus_node_end_position =
-            &(viewport_dimensions * Vector2::new(-0.6, 0.5)).to_variant();
+        let new_focus_node_start_position = &godot_ref
+            .find_node::<Panel>("%screen_off_right")
+            .get("global_position");
+        let screen_center = godot_ref.find_node::<Panel>("%screen_center");
+        let screen_center_focus_position = &screen_center.get("global_position");
+        let current_focus_node_end_position = &godot_ref
+            .find_node::<Panel>("%screen_off_left")
+            .get("global_position");
 
         // disable interaction while moving current focus node away
         // WARN: during animation the scale/position might offset the spawned control blocker
@@ -112,14 +105,13 @@ impl GameScreens {
         mouse_block_control.set("global_position", &Vector2::ZERO.to_variant());
         mouse_block_control.set("mouse_force_pass_scroll_events", &false.to_variant());
 
-        let mut godot_ref = self.to_gd();
         godot_ref.add_child(&new_focus_node);
         new_focus_node.set("global_position", new_focus_node_start_position);
 
         self.change_background(new_background_color, duration);
 
         let mut transition_anim_tween =
-            create_tween(godot_ref, TransitionType::ELASTIC, EaseType::IN_OUT);
+            self.create_tween(TransitionType::ELASTIC, EaseType::IN_OUT);
         // node in center screen -> off screen
         transition_anim_tween.tween_property(
             &current_focus_node,
@@ -149,8 +141,13 @@ impl GameScreens {
         );
         godot_print!(
             "{new_focus_node} moving from {} -> {screen_center_focus_position}",
-            new_focus_node.get("global_position")
+            &new_focus_node.get("global_position")
         );
+
+        // snap new node into center if screen changed during animation
+        transition_anim_tween.signals().finished().connect(move || {
+            new_focus_node.set("global_position", &screen_center.get("global_position"))
+        });
     }
 
     fn _on_start_game_button_pressed(&mut self) {
@@ -158,9 +155,8 @@ impl GameScreens {
             .instantiate()
             .expect("no name_select_screen found to be loaded");
 
-        let mut name_confirm_button: Gd<BaseButton> = name_select_screen
-            .find_node_on("%name_confirm_button")
-            .expect("issue finding %name_confirm_button");
+        let mut name_confirm_button: Gd<BaseButton> =
+            name_select_screen.find_node("%name_confirm_button");
 
         // TODO: better way of connecting scene transition graph
         //let list_screen: Gd<Node> = load::<PackedScene>("uid://djadbqbt76p6g")
@@ -197,38 +193,38 @@ impl GameScreens {
     }
 
     fn change_background(&self, color: Color, duration: f64) {
-        let mut background_tween = create_tween(self.to_gd(), TransitionType::LINEAR, EaseType::IN);
+        let mut background_tween = self.create_tween(TransitionType::LINEAR, EaseType::IN);
         background_tween.tween_property(
-            &self
-                .to_gd()
-                .find_node_on::<ColorRect>("%background")
-                .expect("issue finding %background"),
+            &self.to_gd().find_node::<ColorRect>("%background"),
             "modulate",
             &color.to_variant(),
             duration,
         );
     }
+
+    /// Create a tween with a `TransitionType` and `EaseType` applied
+    /// NOTE: A transition_type of LINEAR will be the same animation no matter the ease type, just a
+    /// constant speed line.
+    ///
+    /// # Panics
+    ///
+    /// Panics if setting the transition or ease type fails.
+    fn create_tween(&self, transition_type: TransitionType, ease_type: EaseType) -> Gd<Tween> {
+        self.base()
+            .get_tree()
+            .expect("failed to create tree")
+            .create_tween()
+            .expect("failed to create tween")
+            .set_trans(transition_type)
+            .expect("failed to set transition for tween")
+            .set_ease(ease_type)
+            .expect("failed to set ease for tween")
+    }
 }
 
-fn create_tween<T: Inherits<Node>>(
-    godot_ref: Gd<T>,
-    transition_type: TransitionType,
-    ease_type: EaseType,
-) -> Gd<Tween> {
-    godot_ref
-        .upcast_ref()
-        .get_tree()
-        .expect("failed to aquire tree")
-        .create_tween()
-        .expect("failed to create tween")
-        .set_trans(transition_type)
-        .expect("failed to set transition for tween")
-        .set_ease(ease_type)
-        .expect("failed to set ease for tween")
-}
-
-trait FindNodeable {
-    fn find_node_on<K: Inherits<Node>>(&self, node_path: &str) -> Result<Gd<K>, String>;
+pub trait FindNodeable {
+    fn try_find_node<T: Inherits<Node>>(&self, node_path: &str) -> Result<Gd<T>, String>;
+    fn find_node<K: Inherits<Node>>(&self, node_path: &str) -> Gd<K>;
 }
 
 impl<T: Inherits<Node>> FindNodeable for Gd<T> {
@@ -245,7 +241,7 @@ impl<T: Inherits<Node>> FindNodeable for Gd<T> {
     /// let button = some_parent_node.find_node_on::<BaseButton>("%button")
     ///     .expect("issue finding %button");
     /// ```
-    fn find_node_on<K: Inherits<Node>>(&self, node_path: &str) -> Result<Gd<K>, String> {
+    fn try_find_node<K: Inherits<Node>>(&self, node_path: &str) -> Result<Gd<K>, String> {
         match self.upcast_ref::<Node>().get_node_or_null(node_path) {
             Some(node_found) => match node_found.try_cast::<K>() {
                 Ok(correct_node) => Ok(correct_node),
@@ -256,5 +252,21 @@ impl<T: Inherits<Node>> FindNodeable for Gd<T> {
             },
             None => Err(format!("{node_path} not found in scene")),
         }
+    }
+
+    /// Access node at given node_path relative to node method is called under.
+    /// NOTE: can use unique names (%node_name)
+    ///
+    /// # Panics
+    ///
+    /// Panics if the node path does not lead to an existing node, or the node's type is invalid.
+    ///
+    /// # Example
+    /// ```
+    /// let button = some_parent_node.find_node::<BaseButton>("%button")
+    /// ```
+    fn find_node<K: Inherits<Node>>(&self, node_path: &str) -> Gd<K> {
+        self.try_find_node(node_path)
+            .expect("issue finding {node_path}")
     }
 }
